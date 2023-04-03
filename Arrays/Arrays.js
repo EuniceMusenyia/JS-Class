804ea1dc1d1fec9b05ed241dbc0e5788057e9b94
student <eunicemusenyia@gmail.com>
2023-04-03 07:52:27 +0300

    first commit

---
+++ Arrays/Arrays.js
@@ -0,0 +1,55 @@
+let num = [24, true, "hello", 38];
+let num2 = new Array (24, true, "hello", 38);
+
+console.log(num);
+console.log(num2);
+
+// adding an element at the end of array
+num.push("fruits");
+console.log(num);
+
+// add element at the start of array
+num.unshift(3);
+console.log(num);
+
+// remove an element at the end 
+num.pop()
+console.log(num);
+
+// remove an element at the start
+num.shift();
+console.log(num);
+
+// given an array of numbers x return an array even where every item in x is multiplied by 5
+// (declare array x ,loop through array x, multiply each item by 5 ,create array even and push items multiplied.)
+ let x = [5, 8, 2, 87, 33, 67];
+ let even = x.map(function(item){
+    return item * 5
+ })
+
+ console.log(even);
+
+//  let result = x.forEach(function(item){
+//     return item * 5
+//  })
+//  console.log(result);
+
+
+ let result = []
+ x.forEach(function(item){
+    result.push(item * 5)
+ })
+ console.log(result);
+
+//  array concuntination
+let newArray = num.concat(x);
+console.log(newArray);
+
+// ...spread operator
+let tests = ["Angela",...x];
+console.log(tests);
+
+let [a, b, ...c] =x;
+console.log({a});
+console.log({b});
+console.log({c});
---
+++ operations-and-conversions/conversions.js
@@ -0,0 +1,8 @@
+let a = "10";
+let b = 10;
+// implicit
+console.log(+a +b);
+
+console.log(a + b);
+// export
+// ({b.log (b))
